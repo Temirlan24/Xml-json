4d1d6bbd4516cb639c5b60fd19631fac2b4f8236
Temirlan Erzatuly <temirlan.erzatuly@gmail.com>
2019-09-16 21:24:29 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp13/Visitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+    public class Visitor
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime EnterTime { get; set; }
+        public DateTime? ExitTime { get; set; } = null;
+    }
+}
